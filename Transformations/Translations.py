0c22b069f6ef696f65895f00c7bce8bc5b4eaffb
threadedmaze <ariadnepet@icloud.com>
2024-12-13 18:48:56 -0700

    Added code

--- Transformations/Translations.py
+++ Transformations/Translations.py
@@ -1,5 +1,3 @@
-#import math
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,35 +6,31 @@ translation_matrix = np.array([[1, 0, 0],
                                [0, 1, 0],
                                [4, 5, 1]])  # shift origin to 4, 5
 # Define the vector
-base_vector = np.array([1, 1, 0])  # goes to (1, 1)
+base_vector = np.array([1, 1, 1])  # goes to (1, 1)
 
 # Perform the matrix-vector multiplication
 result_vector = np.dot(base_vector, translation_matrix)
+print(result_vector)
 
 
-# # Function to plot vectors in 3D
-# def plot_vector_3d(ax, vec, color, label):
-#     ax.quiver(0, 0, 0, vec[0], vec[1], vec[2], color=color, label=label)
-#
+def plot_vector_2d(ax, origin, end, color, label):
+    ax.quiver(origin[0], origin[1], end[0], end[1], angles='xy', scale_units='xy', scale=1, color=color, label=label)
 
 
-# Function to plot vectors in 2D
-def plot_vector_2d(ax, vec, color, label):
-    ax.quiver(0, 0, vec[0], vec[1], angles='xy', scale_units='xy', scale=1, color=color, label=label)
+# Set up the 2D plot
+fig, ax = plt.subplots()
+ax.set_xlim([0, 10])
+ax.set_ylim([0, 10])
+ax.set_aspect('equal')
 
-# Set up the 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim([-5, 5])
-ax.set_ylim([-5, 5])
+# Plot the original vector and the translated vector
+plot_vector_2d(ax, [0, 0], base_vector[:2], 'blue', 'Original Vector')  # From (0, 0) to (1, 1)
+plot_vector_2d(ax, [4, 5], base_vector[:2], 'red', 'Transformed Vector')  # From (4, 5) to (5, 6)
 
-# Plot the original and transformed vectors
-plot_vector_2d(ax, base_vector, 'blue', 'Original Vector')
-#plot_vector_3d(ax, result_vector, 'red', 'Transformed Vector')
-
-# Add legend
+# Add legend, grid, and labels
 plt.legend()
-plt.title("Vector Transformations - Computer Graphics")
+plt.grid(True)
+plt.title("Vector Transformations - Translation")
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.show()
